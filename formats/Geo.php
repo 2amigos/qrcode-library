b36c2d00f09e37179c526620bc6255e8a88785d7
Antonio Ramirez <amigo.cobos@gmail.com>
2014-05-21 20:03:15 +0200

    update copyright  notice

--- formats/Geo.php
+++ formats/Geo.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @copyright Copyright (c) 2013 2amigOS! Consulting Group LLC
+ * @copyright Copyright (c) 2014 2amigOS! Consulting Group LLC
  * @link http://2amigos.us
  * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
  */
