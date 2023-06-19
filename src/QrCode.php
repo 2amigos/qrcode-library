eba3a35a73770c3cda147d4fb2092c2aa725e5ab
mimacala21 <31402757+mimacala21@users.noreply.github.com>
2023-06-19 12:34:48 +0200

    Update QrCode.php
    I got an error 
    
    Parameter $text  in ::__construct() has no class type or default value.
    the $text variable is not given a null value, like the others in the constructor.

--- src/QrCode.php
+++ src/QrCode.php
@@ -80,7 +80,7 @@ class QrCode implements QrCodeInterface
      * @param string|null $errorCorrectionLevel
      * @param WriterInterface|null $writer
      */
-    public function __construct(string $text, string $errorCorrectionLevel = null, WriterInterface $writer = null)
+    public function __construct(string $text = null, string $errorCorrectionLevel = null, WriterInterface $writer = null)
     {
         $this->text = $text;
         $this->errorCorrectionLevel = $errorCorrectionLevel ?: ErrorCorrectionLevelInterface::LOW;
