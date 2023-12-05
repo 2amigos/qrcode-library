946e06c5c58d2b95389351a5e374a4a6483367f9
Jonatas Souza <jonatas.souza@2amigos.us>
2023-12-05 00:22:38 -0300

    setting up codeception/laravel module and laravel package testings

--- routes/laravel.route.php
+++ routes/laravel.route.php
@@ -2,6 +2,6 @@
 use Illuminate\Support\Facades\Route;
 use Da\QrCode\Controllers\LaravelResourceController;
 
-Route::prefix('da-qrcode')->group(function() {
-    Route::get('/build', LaravelResourceController::class);
+Route::prefix('da-qrcode')->name('da-qrcode.')->group(function() {
+    Route::get('/build', LaravelResourceController::class)->name('build');
 });
--- tests/laravel-functional/BootstrapComponentCest.php
+++ tests/laravel-functional/BootstrapComponentCest.php
@@ -2,12 +2,29 @@
 
 class BootstrapComponentCest
 {
-    // tests
-    public function itWorks(FunctionalTester $I)
+    public function assertEndpointWorks(FunctionalTester $I)
     {
         $I->wantTo('Assert endpoint works for qrcode creation');
-        $I->amGoingTo('Call endpoint');
-        $I->amOnRoute('/da-qrcode/build?content=2am. Technologies');
-        $I->seeInSource('asd');
+        $I->amOnRoute('da-qrcode.build', ['content' => '2am. Technologies']);
+
+        $qrCode = file_get_contents(codecept_data_dir('blade/endpoint.png'));
+        $I->seeInSource($qrCode);
+
+        $I->amOnRoute('da-qrcode.build', [
+            'content' => '2am. Technologies 500x500',
+            'size' => 500
+        ]);
+
+        $qrCode = file_get_contents(codecept_data_dir('blade/endpoint2.png'));
+        $I->seeInSource($qrCode);
+
+        $I->amOnRoute('da-qrcode.build', [
+            'content' => '2am. Technologies 500x500',
+            'margin' => 50
+        ]);
+        $source = $I->grabPageSource();
+        file_put_contents(codecept_data_dir('blade/endpoint3.png'), $source);
+        $qrCode = file_get_contents(codecept_data_dir('blade/endpoint3.png'));
+        $I->seeInSource($qrCode);
     }
 }
