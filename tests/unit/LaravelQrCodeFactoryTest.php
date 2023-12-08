1cf57a5dd6f9a30c9eada009401dca066d6c3d96
Jonatas Souza <jonatas.souza@2amigos.us>
2023-12-08 11:39:53 -0300

    moves gradient test

--- tests/unit/LaravelQrCodeFactoryTest.php
+++ tests/unit/LaravelQrCodeFactoryTest.php
@@ -31,62 +31,7 @@ class LaravelQrCodeFactoryTest extends \Codeception\Test\Unit
         LaravelQrCodeFactory::make('2am. Technologies', PngWriter::class);
     }
 
-    public function testCreateGradientQrCode()
-    {
-        $foreground = [
-            'r' => 255,
-            'g' => 0,
-            'b' => 0,
-        ];
-        $foreground2 = [
-            'r' => 0,
-            'g' => 0,
-            'b' => 255,
-            'a' => 30,
-        ];
 
-        $qrCode = LaravelQrCodeFactory::make(
-            '2am. Technologies',
-            null,
-            $foreground,
-            null,
-            null,
-            null,
-            $foreground2
-        )
-        ->writeString();
-
-        $uri = file_get_contents(codecept_data_dir('blade/qrcode-gradient.png'));
-
-        $this->assertEquals(
-            $this->normalizeString($qrCode),
-            $this->normalizeString($uri)
-        );
-
-        $qrCodeRadial = LaravelQrCodeFactory::make(
-            '2am. Technologies',
-            null,
-            $foreground,
-            null,
-            null,
-            null,
-            $foreground2,
-            null,
-            null,
-            null,
-            null,
-            null,
-            Gradient::GRADIENT_RADIAL
-        )
-        ->writeString();
-
-        $uri = file_get_contents(codecept_data_dir('blade/qrcode-gradient-radial.png'));
-
-        $this->assertEquals(
-            $this->normalizeString($qrCodeRadial),
-            $this->normalizeString($uri)
-        );
-    }
 
     protected function normalizeString($string)
     {
