620556e823f4d7b61ccd324a1dce6cf4ca403c86
Jonatas Souza <jonatas.souza@2amigos.us>
2023-12-08 13:37:59 -0300

    fixing cs

--- routes/laravel.route.php
+++ routes/laravel.route.php
@@ -1,5 +1,5 @@
-
 <?php
+
 use Illuminate\Support\Facades\Route;
 use Da\QrCode\Controllers\LaravelResourceController;
 
--- src/Contracts/ColorsInterface.php
+++ src/Contracts/ColorsInterface.php
@@ -76,3 +76,4 @@ interface ColorsInterface
      */
     public function forceUniformRgbColors(): void;
 }
+
